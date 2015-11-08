0f4c6175849d2c3760984ef23e7e64b11faadeec
Sridhar <sridharb@buffalo.edu>
2015-11-08 13:24:49 -0500

    Modified Serial BFS for distances

--- SerialBFS.cpp
+++ SerialBFS.cpp
@@ -1,12 +1,12 @@
-// test program to check graph and time collected 
+// test program to check graph and time collected
 
 #include <ctime>
 #include <iostream>
 #include <fstream>
 #include <vector>
 #include <queue>
-#include <stdio.h>   
-#include <time.h>   
+#include <stdio.h>
+#include <time.h>
 
 using namespace std;
 
@@ -16,8 +16,9 @@ public:
 
 	int n_vertices; //no. of vertices
 	vector<vector<int> > adjList;
+	std::vector<int> sources;
 	bool *check;
-	
+	std::vector<int> d;
 	Graph(int n): adjList(n+1){
 		n_vertices = n;
 		check = new bool[n];
@@ -36,9 +37,16 @@ public:
 };
 
 void Graph::bfs(int n){
-
+	cout<<"worked till here"<<endl;
+	std::vector<int> d(n_vertices);
     for(int i=0;i<n_vertices;i++)
+        {
+
         check[i]=false;
+    	d[i]=-1;
+        }
+
+	cout<<"worked till here"<<endl;
     std::queue<int> q;
     q.push(n);
     while (!q.empty()) {
@@ -48,30 +56,44 @@ void Graph::bfs(int n){
         q.pop();
        vector<int>::iterator iter;
        for(iter = adjList[currentvertex].begin();iter!=adjList[currentvertex].end();++iter) {
-            	if(!check[*iter]){
-                q.push(*iter);
-        	}  
+       		int indx = *iter;
+            	if(d[indx]==-1){
+            		d[indx]=d[currentvertex]+1;
+                	q.push(indx);
+        	}
        }
+    }
+
+    cout<<"done "<<endl;
+    for(int i=0;i<n_vertices;i++)
+    {
+    	cout<<"dval::"<<d[i]<<endl;
     }
 }
 
 Graph ReadfromFile(){
-		
+
 		ifstream in;
-		in.open("/home/heller/Codes-PDP/edges1");	
-		int n,e,i,u,v;
-		in>>n>>e;
+		in.open("/home/heller/Codes-PDP/edges2");
+		int n,e,i,u,v,s;
+		in>>n>>e>>s;
 		Graph grph(n);
-		std::cout<<n<<"---"<<e<<std::endl;
+		// std::cout<<n<<"---"<<e<<std::endl;
 		for(i =1;i<=e;i++){
 			in>>u>>v;
 			grph.addEdge(u,v);
-			std::cout<<u<<"-"<<v<<std::endl;
+			// std::cout<<u<<"-"<<v<<std::endl;
 			}
-		in.close();	
-		
+			int val;
+        for (i =1; i<=s;i++)
+            {
+                in>>val;
+                grph.sources.push_back(val);
+            }
+		in.close();
+
 		return grph;
-		
+
 }
 
 // void print_time(){
@@ -86,7 +108,13 @@ int main(int argc,char *argv[]){
 	clock_t t1, t2;
 	t1 = clock();
 	Graph grph1 = ReadfromFile();
-	grph1.bfs(2);
+
+	for(int i =0;i<grph1.sources.size();i++)
+	{
+		int v = grph1.sources[i];
+		grph1.bfs(v);
+	
+	}
 	t2 = clock();
 	float diff = (((float)t2 - (float)t1) / 1000000.0F ) * 1000;
 	std::cout<<diff<<endl;
