5842c5f674733e3a2a8b0cd8896ebe17ada28441
Sridhar <sridharb@buffalo.edu>
2015-11-16 01:38:02 -0500

    Modified program to show vertices visited in order

--- SerialBFS.cpp
+++ SerialBFS.cpp
@@ -1,4 +1,4 @@
-// test program to check graph and time collected
+// Serial BFS
 
 #include <ctime>
 #include <iostream>
@@ -10,84 +10,118 @@
 
 using namespace std;
 
-class Graph{
+class Graph
+{
 
 public:
 
-	int n_vertices; //no. of vertices
-	vector<vector<int> > adjList;
-	std::vector<int> d;
-	Graph(int n): adjList(n+1){
-		n_vertices = n;
-	}
+    int n_vertices; //no. of vertices
+    vector<vector<int> > adjList;
+    std::vector<int> sources;
+    bool *check;
+    std::vector<int> d;
+    Graph(int n): adjList(n+1)
+    {
+        n_vertices = n;
+        check = new bool[n];
+    }
 
-	void addEdge(int u, int v){
-		adjList[u].push_back(v);
-	}
+    void addEdge(int u, int v)
+    {
+        adjList[u].push_back(v);
+    }
 
-	vector<int>& getadjList(int u){
-		return adjList[u];
-	}
+    vector<int>& getadjList(int u)
+    {
+        return adjList[u];
+    }
 
-	void bfs(int n);
+    void bfs(int n);
 
 };
 
-void Graph::bfs(int n){
-	std::vector<int> d(n_vertices);
-    for(int i=0;i<n_vertices;i++)
-        {
-    	d[i]=-1;
-        }
+void Graph::bfs(int n)
+{
+//	cout<<"worked till here"<<endl;
 
+    for(int i=0; i<n_vertices; i++)
+    {
+
+        check[i]=false;
+    }
+
+    check[n]= true;
     std::queue<int> q;
     q.push(n);
-    d[n]=0;
-    while (!q.empty()) {
+
+    while (!q.empty())
+    {
         int currentvertex = q.front();
+        std::cout<<currentvertex<<std::endl;
         q.pop();
         vector<int>::iterator iter;
-        for(iter = adjList[currentvertex].begin();iter!=adjList[currentvertex].end();++iter) {
-       		int indx = *iter;
-            	if(d[indx]==-1){
-            		d[indx]=d[currentvertex]+1;
-                	q.push(indx);
-        	}
-       }
+        for(iter = adjList[currentvertex].begin(); iter!=adjList[currentvertex].end(); ++iter)
+        {
+            int indx = *iter;
+            if(!check[indx])
+            {
+                check[indx]=true;
+                q.push(indx);
+            }
+        }
     }
+}
 
-    for(int i=0;i<n_vertices;i++)
+Graph ReadfromFile()
+{
+
+    ifstream in;
+    in.open("/home/heller/Codes-PDP/edges7");
+    int n,e,i,u,v,s;
+    in>>n>>e>>s;
+    Graph grph(n);
+    // std::cout<<n<<"---"<<e<<std::endl;
+    for(i =1; i<=e; i++)
     {
-    	cout<<"dval::"<<d[i]<<endl;
+        in>>u>>v;
+        grph.addEdge(u,v);
+        // std::cout<<u<<"-"<<v<<std::endl;
     }
+    int val;
+    for (i =1; i<=s; i++)
+    {
+        in>>val;
+        grph.sources.push_back(val);
+    }
+    in.close();
+
+    return grph;
+
 }
 
-Graph ReadfromFile(){
+// void print_time(){
+// 	// source : stack overflow
+// 	// time_t t =time(0);
+// 	// struct tm * now = localtime(&t);
+// 	// std::cout<< (now->tm_hour<<":"<<now->tm_min<<":"<<now->tm_sec<<now->std::endl;
+// }
 
-		ifstream in;
-		in.open("/home/heller/Codes-PDP/edges5");
-		int n,e,i,u,v,s;
-		in>>n>>e;
-		Graph grph(n);
-		for(i =1;i<=e;i++){
-			in>>u>>v;
-			grph.addEdge(u,v);
-			}
-		in.close();
-		return grph;
+int main(int argc,char *argv[])
+{
+    // print_time();
+    clock_t t1, t2;
+    Graph grph1 = ReadfromFile();
+    t1 = clock();
+
+    int v = 2;
+    grph1.bfs(v);
+
+
+    t2 = clock();
+    float diff = (((float)t2 - (float)t1));
+    float seconds = diff/CLOCKS_PER_SEC;
+	std::cout<<seconds<<endl;
+    // print_time();
+    return 0;
 }
 
-int main(int argc,char *argv[]){
-	clock_t t1, t2;
-	t1 = clock();
-	Graph grph1 = ReadfromFile();
-	
-	// source vertex random
-	int v = 2;
-	grph1.bfs(v);
-	
-	t2 = clock();
-	float diff = (((float)t2 - (float)t1) / 1000000.0F ) * 1000;
-	std::cout<<diff<<endl;
-	return 0;
-}
