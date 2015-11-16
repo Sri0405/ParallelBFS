8e8eb4b625846f94c5c101f1cc4c66bae6338bf1
Sridhar <sridharb@buffalo.edu>
2015-11-16 01:18:42 -0500

    Modified Graph Generator

--- GenerateGraph.py
+++ GenerateGraph.py
@@ -1,6 +1,22 @@
 
 
 import networkx as nx
-G = nx.fast_gnp_random_graph(10,0.5)
-print G.edges()
+#number of vertices
+n = 5000  
+G = nx.fast_gnp_random_graph(n,0.25,directed=True)
+lst = G.edges()
+print n,lst.__len__()
+for i in lst:
+    print str(i[0])+" "+str(i[1])
 
+
+
+
+
+
+
+
+
+
+        
+   
