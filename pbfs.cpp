fa9dff415d2cec1bfaf9e01f619cc976eb4bdb29
Sridhar <sridharb@buffalo.edu>
2015-11-23 18:01:59 -0500

    Made changed working ver

--- pbfs.cpp
+++ pbfs.cpp
@@ -7,15 +7,17 @@
 #include <set>
 #include <cilk/cilk_api.h>
 #include <math.h>
-#include <thread>         
-#include <mutex>          
+#include <thread>
+#include <mutex>
 
 
 std::mutex mtx;
 
+float counts =0;
+
 using namespace std;
 
-const long long int num_processors = 4;
+const long long int num_processors = 1;
 
 float d1=0;
 
@@ -75,7 +77,7 @@ public:
         for (long long int i=1; i<=p; i++)
         {
             if (!(q[i]->empty()))
-            {	
+            {
                 return false;
             }
         }
@@ -146,8 +148,8 @@ public:
 class Graph
 {
 public:
-    long long int n_vertices; 
-    std::vector<std::vector<long long int> > adjList; 
+    long long int n_vertices;
+    std::vector<std::vector<long long int> > adjList;
     std::vector<long long int> d;
     Graph(long long int n):adjList(n + 1)
     {
@@ -179,24 +181,26 @@ public:
                 {
                     continue;
                 }
-                fstream file;
-                file.open("/home/heller/testing/out1.txt", std::ios_base::app);
-                    
+               fstream file;
+               file.open("/home/heller/testing/out1.txt", std::ios_base::app);
+
                 std::vector<long long int>::iterator iter;
                 for (iter = neighbors.begin(); iter != neighbors.end(); ++iter)
                 {
                     long long int indx = *iter;
                     long long int comparev = -1;
-                        
+
                     if (d->at(indx) == comparev)
                     {
                         d->at(indx)= d->at(u)+(long long int)1;
-                        file<<indx;
-                        file<<endl;
+                        // cout<<indx<<endl;
+                       file<<indx;
+                       file<<endl;
+                        counts = counts +1;
                         Qout->insertinto(i,indx);
-                    }      
+                    }
                 }
-                file.close();
+               file.close();
             }
         }
     }
@@ -211,29 +215,31 @@ public:
             }
 
         d[s]=0;
+        counts = counts +1;
+        // cout<<s<<endl;
 	    clock_t t1,t2;
-        ofstream outputfile;
-        outputfile.open("/home/heller/testing/out1.txt");
-        outputfile<<s;
-        outputfile<<endl;
-        outputfile.close();
+       ofstream outputfile;
+       outputfile.open("/home/heller/testing/out1.txt");
+       outputfile<<s;
+       outputfile<<endl;
+       outputfile.close();
         long long int p =num_processors;
 
         CollectionQueues *Qin = new CollectionQueues();
         CollectionQueues *Qout = new CollectionQueues();
 
         Qin->insertinto(1,s);
-       
+
 	    t1= clock();
         while(!Qin->isallempty())
         {
-            long long int n_seg = 4;
+            long long int n_seg = 1;
             long long int ss= Qin->size();
             float df = ss/n_seg;
             df = df + 0.5;
             long long int vv= (long long int)round(df);
             Qin->Sseg = vv;
-          
+
             for (long long int i =1; i<p; i++)
             {
                 cilk_spawn parallelbfsthread(i,Qout,&d,Qin);
@@ -247,12 +253,12 @@ public:
     	t2 = clock();
     	float diff = (((float)t2 - (float)t1));
        	float seconds = diff / CLOCKS_PER_SEC;
-    	d1 = d1 / CLOCKS_PER_SEC;	
+    	d1 = d1 / CLOCKS_PER_SEC;
     	float new_sc = seconds - d1;
-       	fstream ffile;
-        ffile.open("/home/heller/testing/timesf",std::ios_base::app);
-        ffile<<"cores-time:::"<<seconds<<"---"<<d1<<"---"<<new_sc<<endl;
-        ffile.close();
+      	fstream ffile;
+       ffile.open("/home/heller/testing/timesf",std::ios_base::app);
+       ffile<<"cores-time:::"<<seconds<<"---"<<d1<<"---"<<new_sc<<endl;
+       ffile.close();
         delete Qout;
     }
 };
@@ -260,22 +266,19 @@ public:
 
 int main(int argc,char* argv[])
 {
-    ifstream in(argv[1]);
+    ifstream in("/home/heller/testing/edges");
     long long int n, dm, a, b;
     in >> n >> dm;
     Graph g(n);
     while (in >> a >> b)
     {
         g.addEdge(a,b);
-        g.addEdge(b,a);
     }
     in.close();
 
     long long int v= 1;
     g.parallelbfs(v);
- 
+    cout<< "Count is "<< counts<<endl;
     return 0;
 }
-
-
 
